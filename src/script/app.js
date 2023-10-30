ea023bd46a6415f972e7c6ce09243870b625cfc4
Felipe Marques <felipe.comarques1208@gmail.com>
2023-10-29 22:47:47 -0300

    fix: Fix bug allowing consecutive operators

--- src/script/app.js
+++ src/script/app.js
@@ -4,11 +4,13 @@ let buttons = document.querySelectorAll('button');
 let erasebtn = document.querySelector('#erase');
 let clearbtn = document.querySelector('#clear');
 let evaluate = document.querySelector('#evaluate');
+let lastClickedButton = null;
 
 let realTimeScreenValue = []
 
 clearbtn.addEventListener("click", () => {
     realTimeScreenValue = [''];
+    lastClickedButton = null; 
     answerScreen.innerHTML = 0;
     currentInput.className = 'currentInput';
     answerScreen.className = 'answerScreen';
@@ -19,16 +21,20 @@ buttons.forEach((btn) => {
 
     btn.addEventListener("click", () => {
         if (!btn.id.match('erase')) {
-            
-            realTimeScreenValue.push(btn.value)
+            if (btn.classList.contains('fun_btn')) {
+                if (lastClickedButton !== null && lastClickedButton.classList.contains('fun_btn')) {
+                    return; 
+                }
+            }
+            realTimeScreenValue.push(btn.value);
             currentInput.innerHTML = realTimeScreenValue.join('');
 
-            if (btn.classList.contains('num_btn')) {
-                answerScreen.innerHTML = evaluateMath(realTimeScreenValue.join(''));
+            if (btn.classList.contains('fun_btn')) {
+                lastClickedButton = btn;
             }
-
-            if (btn.classList.contains('fun_btn')){
-                btn.disabled
+            if (btn.classList.contains('num_btn')) {
+                lastClickedButton = null;
+                answerScreen.innerHTML = evaluateMath(realTimeScreenValue.join(''));
             }
         }
 
@@ -39,6 +45,7 @@ buttons.forEach((btn) => {
         }
 
         if (btn.id.match('evaluate')) {
+            lastClickedButton = null; 
             currentInput.className = 'answerScreen';
             answerScreen.className = 'currentInput';
             answerScreen.style.color = "white";
